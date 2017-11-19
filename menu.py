e82755d346446254c1fa81c5babe233fe6ce0444
Jędrzej Krauze <jedrzej.krauze@gmail.com>
2017-11-19 18:55:12 +0100

    fancy font added

--- menu.py
+++ menu.py
@@ -5,7 +5,7 @@ import color as col
 class Menu:
     def __init__(self, game, title, options):
         self.game = game
-        self.font = 'System'
+        self.font = 'font/8-BIT WONDER.TTF'
         self.font_color = col.WHITE
         self.font_color_choosed = col.RED
         self.font_color_title = col.GREEN
@@ -40,9 +40,9 @@ class Menu:
 
     def draw(self):
         self.game.screen.blit(
-            pg.font.SysFont(self.font, 60).render(self.title, 1, self.font_color_title), (100, 100))
+            pg.font.Font(self.font, 60).render(self.title, 1, self.font_color_title), (100, 100))
         for i in range(len(self.options)):
             self.game.screen.blit(
-                pg.font.SysFont(self.font, 30).render(self.options[i], 1,
+                pg.font.Font(self.font, 30).render(self.options[i], 1,
                                                       self.font_color if i != self.choose else self.font_color_choosed),
                 (100, 200 + 80 * i))
