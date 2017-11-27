0c4192a2f4a565728a62248c7822e2a9520a325b
Jędrzej Krauze <jedrzej.krauze@gmail.com>
2017-11-27 22:52:21 +0100

    improved block and platform textures

--- block.py
+++ block.py
@@ -1,8 +1,12 @@
 import pygame as pg
+import random
 from image import Image
 
 
 class Block(pg.sprite.Sprite):
+    _sprite_positions = [(0, 0), (0, 3), (0, 5), (0, 9), (0, 10), (0, 22), (0, 23), (0, 27), (0, 31), (1, 3), (1, 8),
+                         (1, 9), (1, 11), (1, 13), (1, 15), (1, 16), (1, 28)]
+
     def __init__(self, game, width, height, x, y):
         super().__init__()
         self.game = game
@@ -10,8 +14,12 @@ class Block(pg.sprite.Sprite):
         self.game.sprites_list.add(self)
         self.width = width
         self.height = height
-        self.image = self.game.ground_sprite.subsurface((0, 6 * 32, 32, 32))
+        self.sprite_pos = self._sprite_positions[random.randint(0, len(self._sprite_positions) - 1)]
+        self.image = self.game.ground_sprite.subsurface((160 * self.sprite_pos[0], 32 * self.sprite_pos[1], 32, 32))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-        self.over_image = Image(self.game, self.game.ground_sprite.subsurface((2 * 32, 6 * 32, 32, 32)), x, y - 20)
+        self.over_image = Image(self.game,
+                                self.game.ground_sprite.subsurface(
+                                    (64 + 160 * self.sprite_pos[0], 32 * self.sprite_pos[1], 32, 32)), x,
+                                y - 20)
--- platform.py
+++ platform.py
@@ -1,8 +1,12 @@
 import pygame as pg
+import random
 from image import Image
+from rock import Rock
 
 
 class Platform(pg.sprite.Sprite):
+    _sprite_positions = [(0, 2), (0, 24), (0, 26), (1, 18), (1, 29), (2, 2), (2, 7), (2, 17)]
+
     def __init__(self, game, width, height, x, y):
         super().__init__()
         self.game = game
@@ -10,8 +14,15 @@ class Platform(pg.sprite.Sprite):
         self.game.sprites_list.add(self)
         self.width = width
         self.height = height
-        self.image = self.game.ground_sprite.subsurface((0, 0, 32, 32))
+        self.sprite_pos = self._sprite_positions[random.randint(0, len(self._sprite_positions) - 1)]
+        self.image = self.game.ground_sprite.subsurface(
+            (32 + 160 * self.sprite_pos[0], 32 * self.sprite_pos[1], 32, 32))
+        self.image = pg.transform.flip(self.image, False, True)
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-        self.over_image = Image(self.game, self.game.ground_sprite.subsurface((2 * 32, 0, 32, 32)), x, y - 20)
+        self.rock = Rock(self.game, x, y)
+        self.over_image = Image(self.game,
+                                self.game.ground_sprite.subsurface(
+                                    (64 + 160 * self.sprite_pos[0], 32 * self.sprite_pos[1], 32, 32)), x,
+                                y - 20)
