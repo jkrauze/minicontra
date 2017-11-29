f28438d72658fef64c18d3f335eea4a8bb849825
Jędrzej Krauze <jedrzej.krauze@gmail.com>
2017-11-29 22:51:10 +0100

    randomize enemy shoot time

--- weapon/soldier_weapon.py
+++ weapon/soldier_weapon.py
@@ -1,3 +1,4 @@
+import random
 from weapon.soldier_bullet import SoldierBullet
 from weapon.weapon import Weapon
 
@@ -5,6 +6,7 @@ from weapon.weapon import Weapon
 class SoldierWeapon(Weapon):
     def __init__(self, game):
         super().__init__(game, 60)
+        self.state = -random.randint(0,60)
 
     def shoot(self, x, y, shoot_direction):
         self.game.enemy_bullets_list.add(SoldierBullet(self.game, x, y, shoot_direction))
