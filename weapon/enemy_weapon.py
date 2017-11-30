9d0acefacda6c2c68b9c9692ff9f4accc29e24cc
Jędrzej Krauze <jedrzej.krauze@gmail.com>
2017-11-30 09:01:51 +0100

    automatic shooting better handled

--- weapon/enemy_weapon.py
+++ weapon/enemy_weapon.py
@@ -5,11 +5,7 @@ from weapon.weapon import Weapon
 class EnemyWeapon(Weapon):
     def __init__(self, game, frequency):
         super().__init__(game, frequency)
-        self.state = -random.randint(0, frequency)
+        self.state = random.randint(0, frequency)
 
     def shoot(self, x, y, shoot_direction):
         raise NotImplementedError('shoot method must be implemented by weapon subclass')
-
-    def handle_shooting(self, x, y, shoot_direction):
-        super().handle_shooting(x, y, shoot_direction)
-        super().start_shooting()
